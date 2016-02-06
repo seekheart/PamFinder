d5e9c908073d1c2b5124836f9e258bc53c2618c9
Mike Tung <miketung2013@gmail.com>
2016-02-06 14:54:57 -0500

    Made perl script to download and fetch yeast chromosome Data, requires install of EUtilities for BioPerl

---
+++ downloadYeastData.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Getopt::Long;
+use Pod::Usage;
+use Bio::DB::EUtilities;
+use Bio::SeqIO;
+use feature qw(say);
+
+
+#Make the options
+my $infile;
+my $out;
+my $email;
+my $usage = "\n$0\n
+
+Usage:
+-infile           Input File with Genbank/RefSeq Ids
+-email            Your Email
+-out                Output Base File Name
+-verbose          Display output, off by default
+-help             Display This
+
+";
+
+GetOptions(
+                    "infile=s"          =>          \$infile,
+                    "email=s"         =>           \$email,
+                    "out=s"             =>           \$out,
+                      help                =>          sub{pod2usage($usage);},
+                    );
+
+#check required options are met
+unless($infile && $email && $out){
+    die "$usage";
+}
+
+#Make output directory
+my $currentDir = `pwd`;
+chomp $currentDir;
+my $dirName = $currentDir . "/output";
+unless (-e $dirName){
+    `mkdir -p $dirName`;
+}
+
+
+
+#Process and extract out the RefSEq IDs
+open(my $fh, '<', $infile) or die "Invalid File!";
+my @ids = ();
+while (<$fh>){
+    chomp;
+    if ($_ =~ m/(NC.{9})/) {
+        push @ids, $1;
+    }
+}
+close($fh);
+
+#Make a factory to fetch fastas
+foreach my $id (@ids){
+    my $factory = Bio::DB::EUtilities->new(
+                                                                    -eutil    =>  'efetch',
+                                                                    -rettype => 'fasta',
+                                                                    -db       =>  'nucleotide',
+                                                                    -id        =>   $id,
+                                                                    -email   =>  $email,
+                                                                    -tool      =>   'get_refseq_id'
+                                                                    );
+    #output file to directory
+    my $file = $dirName . '/' . $out . '.' . $id . '.fasta';
+    if (-s $file){
+        say STDERR "File Exists Skipping...";
+    }
+    else{
+        $factory->get_Response( -file => $file);
+        say STDERR "Fetching data from NCBI for $id";
+        sleep(3);
+    }
+}
